18fe2de1bb88c6c64f61ed61fdfee2f8c09e9b89
harvy_xu <xuweiroot@163.com>
2017-09-23 09:56:17 +0000

    add greenlet_mem_leak

---
+++ greenlet/greenlet_mem_leak_v2.py
@@ -0,0 +1,28 @@
+from greenlet import greenlet, GreenletExit
+
+huge = []
+
+def show_leak():
+    def test1():
+	gr2.switch()
+
+    def test2():
+	huge.extend([x*x for x in range(100)])
+	try:
+	    gr1.switch()
+	finally:
+	    print "finish switch del huge"
+            del huge[:]
+
+    gr1 = greenlet(test1)
+    gr2 = greenlet(test2)
+    gr1.switch()
+    gr1 = gr2 = None
+    print "length of huge is zero ? %s" % len(huge)
+
+if __name__ == '__main__':
+    show_leak()
+
+#result output
+#finish switch del huge
+#length of huge is zero ? 0
